dafbf8a05816cb69b5bb298dee6385a3f958f7a3
stevey52 <49789919+stevey52@users.noreply.github.com>
2022-04-22 12:00:19 +0300

    streams

--- static/java/detailView.js
+++ static/java/detailView.js
@@ -2,3 +2,11 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
